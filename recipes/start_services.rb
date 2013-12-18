8481ba0ce2007c135c42504dfa9f233ad87a3c41
Elias Torres <elias@torrez.us>
2013-12-18 16:18:32 -0500

    a

--- recipes/start_services.rb
+++ recipes/start_services.rb
@@ -1,15 +1,15 @@
 bash "starting zookeeper" do
   code   <<-EOH
+    /home/vagrant/mesos/build/3rdparty/zookeeper-3.3.4/bin/zkServer.sh stop || true
     /home/vagrant/mesos/build/3rdparty/zookeeper-3.3.4/bin/zkServer.sh start
   EOH
   action :run
-  not_if ("pgrep zoo")
 end
 
 bash "starting mesos" do
   code   <<-EOH
-    mesos-start-cluster.sh > /tmp/mesos-start.log
+    mesos-stop-cluster.sh || true
+    mesos-start-cluster.sh 
   EOH
   action :run
-  not_if ("pgrep mesos")
 end
