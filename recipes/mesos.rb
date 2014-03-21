8bd72085bb2bf1c762f0ef17fe62cdee21c8d7db
tpetr <tpetr@hubspot.com>
2014-03-20 22:07:47 -0400

    write to /etc/mesos-master/ip instead of the env var

--- recipes/mesos.rb
+++ recipes/mesos.rb
@@ -1,12 +1,18 @@
 private_ip = node[:network][:interfaces][:eth1][:addresses].detect{|k,v| v[:family] == "inet" }.first
 
+directory '/etc/mesos-master' do
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+file '/etc/mesos-master/ip' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+  content private_ip
+end
+
 include_recipe "mesos::master"
 include_recipe "mesos::slave"
-
-ruby_block "insert_line" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/default/mesos")
-    file.insert_line_if_no_match("/MESOS_ip=/", "MESOS_ip=#{private_ip}")
-    file.write_file
-  end
-end
