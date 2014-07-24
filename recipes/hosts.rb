f7806549ea1c2536ce3e1ba8221847d53b6ef9c3
tpetr <tpetr@hubspot.com>
2014-07-24 10:23:17 -0400

    tweak

--- recipes/hosts.rb
+++ recipes/hosts.rb
@@ -1,8 +1,8 @@
-hostfile_entry '127.0.1.1' do
+hostsfile_entry '127.0.1.1' do
   action :remove
 end
 
-hostfile_entry node[:mesos][:common][:ip] do
+hostsfile_entry node[:mesos][:common][:ip] do
   hostname node['hostname']
   action :create
 end
