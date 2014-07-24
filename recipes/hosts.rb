890252285a4eb7b296c6189e017ec8da6a9d7593
tpetr <tpetr@hubspot.com>
2014-07-24 10:18:13 -0400

    fix

--- recipes/hosts.rb
+++ recipes/hosts.rb
@@ -1,10 +1,8 @@
-include_recipe 'hostsfile'
-
 hostfile_entry '127.0.1.1' do
   action :remove
 end
 
 hostfile_entry node[:mesos][:common][:ip] do
   hostname node['hostname']
-  action :update
+  action :create
 end
