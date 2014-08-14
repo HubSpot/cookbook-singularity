48347e8b6ae78d336a49cb55c30af60c2c461d9d
Grigorios Chomatas <gchomatas@hubspot.com>
2014-08-14 15:06:36 +0100

    fixed installation of oracle java 7 jdk

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,3 +18,8 @@ default[:mesos] = {
   },
   :slave => {}
 }
+
+override['java']['install_flavor'] = "oracle"
+override['java']['jdk_version'] = "7"
+override['java']['oracle']['accept_oracle_download_terms'] = true
+override['java']['set_default'] = true
